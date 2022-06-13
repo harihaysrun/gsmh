7ccee6585cac97787484e73386f0473f9db4b8e9
harihaysrun <nursyahirah98@outlook.com>
2022-06-13 05:43:02 +0000

    highlight section on scroll

--- gsmh/src/App.vue
+++ gsmh/src/App.vue
@@ -8,18 +8,18 @@
       </div>
     </nav>
     <nav class="sec-nav">
-      <div class="container py-3 d-flex flex-row flex-nowrap overflow-scroll text-center justify-content-lg-center">
-        <div class="sec-nav-link col-3 col-md-auto pe-md-5" v-scroll-to="'#test'">About Us</div>
-        <div class="sec-nav-link col-4 col-md-auto pe-md-5" v-scroll-to="'#programmes'">Programmes</div>
-        <div class="sec-nav-link col-4 col-md-auto pe-md-5" v-scroll-to="'#testimonials'">Testimonials</div>
-        <div class="sec-nav-link col-2 col-md-auto pe-md-5" v-scroll-to="'#events'">Events</div>
-        <div class="sec-nav-link col-2 col-md-auto pe-md-5" v-scroll-to="'#faq'">FAQ</div>
-        <div class="sec-nav-link col-3 col-md-auto pe-md-5" v-scroll-to="'#contact'">Contact Us</div>
+      <div class="container p-0 d-flex flex-row flex-nowrap overflow-scroll text-center justify-content-lg-center">
+        <div class="sec-nav-link py-3 px-4 col-md-auto text-nowrap" v-scroll-to="'#about'" @click="scrollToSection(1)" :class="{'sec-nav-link-active': section === 1}">About Us</div>
+        <div class="sec-nav-link py-3 px-4 col-md-auto text-nowrap" v-scroll-to="'#programmes'" @click="scrollToSection(2)" :class="{'sec-nav-link-active': section === 2}">Programmes</div>
+        <div class="sec-nav-link py-3 px-4 col-md-auto text-nowrap" v-scroll-to="'#testimonials'" @click="scrollToSection(3)" :class="{'sec-nav-link-active': section === 3}">Testimonials</div>
+        <div class="sec-nav-link py-3 px-4 col-md-auto text-nowrap" v-scroll-to="'#events'" @click="scrollToSection(4)" :class="{'sec-nav-link-active': section === 4}">Events</div>
+        <div class="sec-nav-link py-3 px-4 col-md-auto text-nowrap" v-scroll-to="'#faq'" @click="scrollToSection(5)" :class="{'sec-nav-link-active': section === 5}">FAQ</div>
+        <div class="sec-nav-link py-3 px-4 col-md-auto text-nowrap" v-scroll-to="'#contact'" @click="scrollToSection(6)" :class="{'sec-nav-link-active': section === 6}">Contact Us</div>
       </div>
     </nav>
 
     <Header/>
-    <About id="test"/>
+    <About id="about"/>
     <Programmes id="programmes"/>
     <Testimonials id="testimonials"/>
     <Events id="events"/>
@@ -49,18 +49,41 @@ export default {
   components: {
     Header, About, Programmes, Testimonials, Events, FAQ, Bailey, Contact, Footer
   },
+  data(){
+    return{
+      'section': 0
+    }
+  },
+  mounted(){
+    window.addEventListener('scroll', this.onscroll)
+  },
   methods:{
-    scrollToElement:function(){
-      // console.log(this.About.getBoundingClientRect().width)
-      // console.log(document.getElementById('test'))
-      // console.log(this.$refs.aboutSection)
-      // this.$refs.aboutSection.scrollIntoView()
-      // window.scrollTo({
-      //   top: 100,
-      //   left: 100,
-      //   behavior: 'smooth'
-      // });
-      // this.$refs.aboutSection.scrollIntoView({behavior: 'smooth'});
+    onscroll:function(){
+      console.log(window.scrollY, document.body.scrollHeight)
+      if(window.scrollY > document.getElementById('about').offsetTop - 500){
+        this.section = 1;
+      } else{
+        this.section = 0;
+      }
+      if(window.scrollY > document.getElementById('programmes').offsetTop - 500){
+        this.section = 2;
+      }
+      if(window.scrollY > document.getElementById('testimonials').offsetTop - 500){
+        this.section = 3;
+      }
+      if(window.scrollY > document.getElementById('events').offsetTop - 500){
+        this.section = 4;
+      }
+      if(window.scrollY > document.getElementById('faq').offsetTop - 500 ){
+        this.section = 5;
+      }
+      if(window.scrollY > document.getElementById('contact').offsetTop - 500 || window.scrollY >= document.body.scrollHeight - window.innerHeight){
+        this.section = 6;
+      }
+    },
+    scrollToSection:function(sectionNo){
+      console.log(sectionNo)
+      this.section = sectionNo;
     }
   }
 }
@@ -107,6 +130,7 @@ nav{
   color:white; 
   position: sticky;
   top:0;
+  z-index:10;
 }
 
 .sec-nav .container::-webkit-scrollbar{
@@ -122,6 +146,17 @@ nav{
 
 .logo{
   width:60px;
+}
+
+.sec-nav-link{
+  box-sizing: border-box;
+  border-bottom:2px solid transparent;
+  cursor:pointer;
+}
+
+.sec-nav-link:hover, .sec-nav-link-active{
+  font-weight:700;
+  border-bottom:2px solid white;
 }
 
 img{
--- gsmh/src/main.js
+++ gsmh/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 const VueScrollTo = require('vue-scrollto')
+// const VueScrollactive = require('vue-scrollactive')
+// import VueScrollactive from 'vue-scrollactive';
 
+// createApp(App).use(VueScrollactive)
 createApp(App).use(VueScrollTo, {
                     container: "body",
                     duration: 300,
